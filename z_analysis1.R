ca98875446fe053c337c3680c0da9ec392d18c70
Sanmati Dugad <106019002+sanmatidugad@users.noreply.github.com>
2023-10-27 09:51:45 -0400

    z_analysis.R

--- z_analysis1.R
+++ z_analysis1.R
@@ -1,3 +1,13 @@
+## MAINTAINER: Sanmati Dugad
+
+## Command Line:
+# Rscript z_analysis.v1.R \
+# -i <test>_normalized.txt \
+# -s <test>-gene_states.csv \
+# -gc <test>_subsampled_<reads>.gc_totals.csv \
+# -n normal_count {numeric value}
+# -a actionable_data {gene export file}
+
 rm(list=ls())    #clear the environment in R. Remove all objects (variables, functions, etc.) from the current workspace.
 Sys.setenv(TZ='EDT')
 
@@ -9,11 +19,10 @@ for (each in 1:length(ReQ_Modules)) {
   }
 }
 
-#invisible(lapply(ReQ_Modules, library, character.only = TRUE))
-
 suppressWarnings(library(tidyr))
 suppressWarnings(library(data.table))
 suppressWarnings(library(matrixStats))
+suppressWarnings(library(utils))
 
 args=commandArgs(trailingOnly = T)
 
@@ -22,81 +31,73 @@ states_data = NULL    #gene states
 gene_counts = NULL    #gene counts file from subsampled output
 actionable_data = NULL    #actionable gene list
 normal_count = NULL    #number of normal samples
-tumor_sample = NULL    #tumor sample name
 
-
-if ("-i" %in% args ){ #&& "-s" %in% args && "-gc" %in% args && "-n" %in% args && "-t" %in% args && "-a" %in% args ) {    
+if ("-i" %in% args ){ #&& "-s" %in% args && "-gc" %in% args && "-n" %in% args && "-a" %in% args ) {    
   normalized_data_index = which(args == "-i")
-  states_data_index = which(args == "-s")    #Getting index of the "--input" option
+  states_data_index = which(args == "-s")
   gene_counts_index = which(args == "-gc")
   normal_count_index = which(args == "-n")
-  tumor_sample_index = which(args == "-t")
   actionable_data_index = which(args == "-a")
   
   normalized_data = args[normalized_data_index + 1] 
   states_data =  args[states_data_index + 1] 
   gene_counts = args[gene_counts_index + 1]
-  normal_count = as.numeric(args[normal_count_index + 1])
-  tumor_sample = args[tumor_sample_index + 1]
+  normal_count = args[normal_count_index + 1]
   actionable_data = args[actionable_data_index + 1]
 }
 
-normalized_data = "test1_normalized.csv"
-setwd("/home/data/Dropbox (Genomic Expression)/GEx Bioinformatics/12. OneRNA Secondary Pipeline/Sanmati Normalization Experiments/Z_Analysis/")
+normalized_input = read.table(normalized_data, row.names = 1)
+normal_count = as.numeric(normal_count)
+tumor_sample = colnames(normalized_input[,(normal_count+1):(ncol(normalized_input)), drop = FALSE])
 
-#print(paste("Normalized File Name: ", normalized_data))
-normalized_input = read.csv(normalized_data, row.names = 1, sep = ",")
-#print(paste("Read Normalized File"))
-states_input = read.csv(states_data, row.names = 1)
-#print(paste("Read Gene States File"))
-#print(paste(tumor_sample))
+main = function(normalized_data, states_input, gene_counts, actionable_data, i){
+  normalized_input = read.table(normalized_data, row.names = 1)
+  states_input = read.csv(states_data, row.names = 1)
+  counts_input = read.csv(gene_counts, row.names = 1)
+  actionable_input = read.csv(actionable_data)
+  normal_count = as.numeric(normal_count)
+  
+  normalized_input[, c(i)] = round(normalized_input[, c(i)], 3)    # round up tumor data point to 
+  normalized_input$Normalized_Per_Million = round((normalized_input[,c(i)] * 1000000) / counts_input[c(i), c("new_totals")] , 3)    #Per Million Normalized Data
+  normalized_input$Normal_Mean = round(rowMeans(normalized_input[,1:normal_count]), 3)  # calculating Normal Means
+  normalized_input$Normal_Median = round(apply(normalized_input[,1:normal_count], MARGIN = 1, median), 4)    #calculating Normal Medians
+  normalized_input$Normal_SD = round(apply(normalized_input[,1:normal_count], 1, sd) , 3)    #calculating Standard Deviation 
+  normalized_input$Normal_SD[normalized_input$normal_SD == 0] = 1    # replacing standard deviation of zero by 1
+  normalized_input$Z_score = round((normalized_input[, c(i)] - normalized_input$Normal_Mean) / normalized_input$Normal_SD , 3)   #calculating Z-scores
+  normalized_input$Foldchange = round(normalized_input[, c(i)] / normalized_input$Normal_Mean, 3)    # calculate foldchange values
+  
+  normalized_input$Z_score[is.na(normalized_input$Z_score)] = 0    #performing for a single vector at a time. cost of is.na function is n^2
+  normalized_input$Z_score[normalized_input$Z_score == Inf] = 0
+  normalized_input$Z_score[normalized_input$Z_score == -Inf] = 0
+  
+  normalized_input$Foldchange[is.na(normalized_input$Foldchange)] = 0
+  normalized_input$Foldchange[normalized_input$Foldchange == Inf] = 0
+  normalized_input$Foldchange[normalized_input$Foldchange == -Inf] = 0
+  
+  states_tumor = states_input[, c("SYMBOL", i), drop = FALSE]    #selecting SYMBOL column and tumor sample column in gene state data
+  colnames(states_tumor) = c("SYMBOL", "Gene_State")    #renaming tumor sample in gene state data    
+  normalized_input.1 = merge(normalized_input, states_tumor, by = "row.names", all.x = TRUE)    #merging normalized data table with gene state tumor data
+  
+  #Adding False Positive and False Negative Check
+  normalized_input.1$Potential_FP_or_FN = NA
+  #FALSE POSITIVE: median normal < 10, normalized expression < 10, Aberrantly Called (Very High, High, Low, Very Low)
+  normalized_input.1$Potential_FP_or_FN <- ifelse(normalized_input.1[, c(i)] < 10 & normalized_input.1[, c("Normal_Median")] < 10 & normalized_input.1[, c("Gene_State")] != "NORMAL", "+", normalized_input.1$Potential_FP_or_FN)
+  #FALSE NEGATIVE: median normal < 10, normalized expression < 10, Not Aberrantly Called (Normal)
+  normalized_input.1$Potential_FP_or_FN <- ifelse(normalized_input.1[, c(i)] < 10 & normalized_input.1[, c("Normal_Median")] < 10 & normalized_input.1[, c("Gene_State")] == "NORMAL", "-", normalized_input.1$Potential_FP_or_FN)
+  normalized_input.1$Potential_FP_or_FN[is.na(normalized_input.1$Potential_FP_or_FN)] = 0
+  rownames(normalized_input.1) = normalized_input.1[,1]
+  
+  normalized_input.2 = normalized_input.1[, c("SYMBOL", i, "Normalized_Per_Million","Normal_Mean", "Normal_Median", "Normal_SD", "Z_score", "Foldchange", "Gene_State", "Potential_FP_or_FN")]    #selecting columns for final output
+  
+  write.csv(normalized_input.2, file = paste(i, "-z_stats.csv", sep = ""), row.names = T)  
+  
+  # Only Actionable Genes.
+  a = actionable_input[trimws(actionable_input$gene) %in% trimws(normalized_input.2$SYMBOL),]
+   normalized_input.3 = normalized_input.2[normalized_input.2$SYMBOL %in% a$gene, ]
+   write.csv(normalized_input.3, paste(i, "-z_stats-actionable.csv", sep = ""))
 
-counts_input = read.csv(gene_counts, row.names = 1)
-#print(paste("Read Gene Counts File"))
+}
 
-actionable_input = read.csv(actionable_data)
-#print(paste("Read Actionable File"))
-
-
-#head(states_input)
-#colnames(normalized_input)
-normal_count = 23
-dim(normalized_input)
-
-tumor_sample = colnames(normalized_input)[normal_count+1:ncol(normalized_input)]
-normalized_input[, c(tumor_sample)] = round(normalized_input[, c(tumor_sample)], 3)    # round up tumor data point to 
-normalized_input$Normalized_Per_Million = round((normalized_input[,c(tumor_sample)] * 1000000) / counts_input[c(tumor_sample), c("new_totals")] , 3)
-normalized_input$Normal_Mean = round(rowMeans(normalized_input[,1:normal_count]), 3)  # calculating Normal Means
-normalized_input$Normal_Median = round(apply(normalized_input[,1:normal_count], MARGIN = 1, median), 4)    #calculating Normal Medians
-normalized_input$Normal_SD = round(apply(normalized_input[,1:normal_count], 1, sd) , 3)    #calculating Standard Deviation 
-normalized_input$Normal_SD[normalized_input$normal_SD == 0] = 1    # replacing standard deviation of zero by 1
-normalized_input$Z_score = round((normalized_input[, c(tumor_sample)] - normalized_input$Normal_Mean) / normalized_input$Normal_SD , 3)   #calculating Z-scores
-normalized_input$Foldchange = round(normalized_input[, c(tumor_sample)] / normalized_input$Normal_Mean, 3)    # calculate foldchange values
-normalized_input[is.na(normalized_input)] = 0    # replace all NA's and NAN's by 0
-normalized_input[normalized_input == Inf] = 0    # replacing Inf by 0
-normalized_input[normalized_input == -Inf] = 0
-
-states_tumor = states_input[, c("SYMBOL", tumor_sample), drop = FALSE]    #selecting SYMBOL column and tumor sample column in gene state data
-colnames(states_tumor) = c("SYMBOL", "Gene_State")    #renaming tumor sample in gene state data    
-normalized_input.1 = merge(normalized_input, states_tumor, by = "row.names", all.x = TRUE)    #merging normalized data table with gene state tumor data
-head(normalized_input.1)
-
-#Adding False Positive and False Negative Check
-normalized_input.1$Potential_FP_or_FN = NULL
-#FALSE POSITIVE: median normal < 10, normalized expression < 10, Aberrantly Called (Very High, High, Low, Very Low)
-normalized_input.1[which(normalized_input.1[,c(tumor_sample)] < 10 & normalized_input.1[,c("Normal_Median")] < 10 & normalized_input.1[,c("Gene_State")] != "NORMAL"), c("Potential_FP_or_FN") ] = "+"
-#FALSE NEGATIVE: median normal < 10, normalized expression < 10, Not Aberrantly Called (Normal)
-normalized_input.1[which(normalized_input.1[,c(tumor_sample)] < 10 & normalized_input.1[,c("Normal_Median")] < 10 & normalized_input.1[,c("Gene_State")] == "NORMAL"), c("Potential_FP_or_FN")] = "-"
-normalized_input.1[is.na(normalized_input.1)] = 0
-
-rownames(normalized_input.1) = normalized_input.1[,1]    # changing back rownames to original
-
-## Selecting Columns for Output File
-normalized_input.2 = normalized_input.1[, c("SYMBOL", tumor_sample, "Normalized_Per_Million","Normal_Mean", "Normal_Median", "Normal_SD", "Z_score", "Foldchange", "Gene_State", "Potential_FP_or_FN")]    #selecting columns for final output
-write.csv(normalized_input.2, paste(tumor_sample, "-z_stats.csv", sep = ""))  
-
-## Only Actionable Genes.
-a = actionable_input[trimws(actionable_input$gene) %in% trimws(normalized_input.2$SYMBOL),]
-normalized_input.3 = normalized_input.2[normalized_input.2$SYMBOL %in% a$gene, ]
-write.csv(normalized_input.3, paste(tumor_sample, "-z_stats-actionable.csv", sep = ""))
-
+for(i in tumor_sample){
+  main(normalized_data, states_input, gene_counts, actionable_data, i)
+}
